c97397bb7372f0c2de1d4aebffce2d40a7d69ce0
Subhadeep Sarkar <thehitmansubhadeepsarkar@gmail.com>
2023-12-07 21:16:38 +0530

    sec_com

--- BlueSky.py
+++ BlueSky.py
@@ -5,5 +5,4 @@ from bluesky import traf
 
 
 if __name__ == "__main__":
-    # traf.cre("WB10", "A225", 20.171466, 77.113148, 130, "FL410", 485)
     sys.exit(main())
--- hello.py
+++ hello.py
@@ -1,18 +1,29 @@
+def gec(coordinates):
+    x_sum = sum(loc[0] for loc in coordinates)
+    y_sum = sum(loc[1] for loc in coordinates)
+    x_cord = x_sum / len(coordinates)
+    y_cord = y_sum / len(coordinates)
+
+    return x_cord, y_cord
+
 def ecmb(coordinate):
-    euclidean_customer = [loc for loc in coordinate if loc[1] >= 74 and loc[1] <= 79.99]
-    manhattan_customer = [loc for loc in coordinate if loc[1] >= 80 and loc[1] <= 83.00]
-
-    x_axis = sum(customer[0] for customer in coordinate) / len(coordinate)
-    border = max(customer[0] for customer in euclidean_customer)
-
-    y_axis = (sum(customer[1] for customer in euclidean_customer) + (len(manhattan_customer) * border)) / len(coordinate)
-
-    final_centroid = [x_axis, y_axis]
-
+    longitude = 0
+    latitude = 0
+    k=88.4354
+    test = gec(coordinate)
+    print("Test",test)
+    for loc in coordinate:
+        if loc[1] <= k:
+            longitude+= loc[1]
+        else:
+            longitude+= k
+        latitude+= loc[0]
+    y_axis = longitude/len(coordinate)
+    x_axis = latitude/len(coordinate)
     return x_axis, y_axis
 
 if __name__ == '__main__':
-    delivary_loc = [(21.4, 75.7),(23.5, 78.4),(22.3, 79.2),(21.0, 82.8),(22.2, 82.9)]
+    delivary_loc = [(22.597, 88.4203),(22.6129, 88.4209),(22.6032, 88.4417),(22.5946, 88.4566),(22.5846, 88.4105)]
     x_cord, y_cord = ecmb(delivary_loc)
 
     print(x_cord, y_cord)
